dc6997b0605799b6d1c0507d3fe0c178d0558458
laibaajab <laibaajab@gmail.com>
2024-08-29 04:34:27 -0400

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'super-secret'  # Change this to a random secret key
